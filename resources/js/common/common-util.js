ba4d80bdf33ea5c159998ebfbb4da11fe8c3ea0e
JimShawn <495629394@qq.com>
2017-05-11 17:40:06 +0800

    chang the ip

--- resources/js/common/common-util.js
+++ resources/js/common/common-util.js
@@ -9,7 +9,7 @@ commonUtil.factory("commonUtil",function (cityJson) {
     var currentTabIndex = {};
     var currentItem = "";
 
-    var imageHost = "http://106.14.3.254:80/";
+    var imageHost = "http://139.224.135.179:80/";
     var serverHost = "/cheanxin/";
 
     factory.getServerHost = function () {
