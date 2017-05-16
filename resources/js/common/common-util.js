6b8176d777dc710ccfa36d8f962497c2c2082a59
hezudaopp <hezudaopp@163.com>
2017-05-16 16:15:40 +0800

    release list: bug fixed

--- resources/js/common/common-util.js
+++ resources/js/common/common-util.js
@@ -10,7 +10,7 @@ commonUtil.factory("commonUtil",function (cityJson) {
     var currentItem = "";
 
     var imageHost = "http://139.224.135.179:80/";
-    var serverHost = "http://localhost:8081/cheanxin/";
+    var serverHost = "/cheanxin/";
 
     factory.getServerHost = function () {
         return serverHost;
